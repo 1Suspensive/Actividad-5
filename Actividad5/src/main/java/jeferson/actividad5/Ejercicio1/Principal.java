05f50b6b58c9b25593c1571704c2a99d0718ed81
1Suspensive <jeospinaga@unal.edu.co>
2023-06-14 16:16:35 -0500

    Ejercicio 1

---
+++ Actividad5/src/main/java/jeferson/actividad5/Ejercicio1/Principal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package jeferson.actividad5.Ejercicio1;
+
+/**
+ *
+ * @author JEFERSON
+ */
+public class Principal {
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal = new VentanaPrincipal();
+        miVentanaPrincipal.setVisible(true);
+    }
+}
