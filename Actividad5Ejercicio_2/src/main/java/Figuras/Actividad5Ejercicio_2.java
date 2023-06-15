77055c62c0386b561d458a7eeadbceab50184408
tvargasl <126362144+tvargasl@users.noreply.github.com>
2023-06-15 15:34:22 -0500

    Add files via upload

---
+++ Actividad5Ejercicio_2/src/main/java/Figuras/Actividad5Ejercicio_2.java
@@ -0,0 +1,11 @@
+package Figuras;
+
+public class Actividad5Ejercicio_2 {
+
+    public static void main(String[] args) {
+        VentanaPrincipal iu = new VentanaPrincipal();
+        iu.setVisible(true);
+        iu.setResizable(false);
+        
+    }
+}
